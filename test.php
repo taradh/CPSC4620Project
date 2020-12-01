f75b5463dac052912081a7716bebbfacb80496c3
carrinj <carrinj@g.clemson.edu>
2020-11-30 21:47:25 -0500

    Adding pictures and book details

---
+++ test.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- Unique CSS  -->
+    <link  href = "unique-css.css" rel="stylesheet" type="text/css">
+    <title>Library Management System</title>
+  </head>
+  <body>
+    <header class="index-header">
+      <h1 style = "text-align: center;">Library Management System</h1>
+    </header>
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+      </ol>
+      <div class="carousel-inner carousel-content">
+        <div class="carousel-item">
+
+          <div class="container">
+            <div class="carousel-caption">
+              <div class="row">
+                  <div class="col-sm-3">
+                      <img src="pictures/fiction/tkamb.jpg" alt="To Kill A Mocking Bird">
+                        <h3 style="text-align: center;"><br>To Kill A Mocking Bird</h3>
+                    </div>
+                  <div class="col-sm-3">
+                    <img src="pictures/fiction/toaoi.jpg" alt="The One and Only Ivan">
+                </div>
+                <div class="col-sm-3">
+                    <img src="pictures/fiction/tlkoe.jpg" alt="The Last Kids On Earth">
+                </div>
+                <div class="col-sm-3">
+                    <img src="pictures/fiction/restart.jpg" alt="Restart">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="carousel-item active">
+
+            <div class="carousel-caption">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img src="pictures/fiction/tkamb.jpg" alt="To Kill A Mocking Bird">
+                          <h3 style="text-align: center;"><br>To Kill A Mocking Bird</h3>
+                      </div>
+                    <div class="col-sm-3">
+                      <img src="pictures/fiction/toaoi.jpg" alt="The One and Only Ivan">
+                      <h3 style="text-align: center;"><br>The One and Only Ivan</h3>
+                  </div>
+                  <div class="col-sm-3">
+                      <img src="pictures/fiction/tlkoe.jpg" alt="The Last Kids On Earth">
+                      <h3 style="text-align: center;"><br>The Last Kids On Earth</h3>
+                  </div>
+                  <div class="col-sm-3">
+                      <img src="pictures/fiction/restart.jpg" alt="Restart">
+                      <h3 style="text-align: center;"><br>Restart</h3>
+                  </div>
+                </div>
+              </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper)
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+-->
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS     -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+  </body>
+</html>
